2d0570546044ecaff0c79f653877a18ea4366e91
Afumett0 <ahmeteren1905.aey@gmail.com>
2026-05-05 17:46:28 +0300

    feat: add borrow_records table with policies, triggers and view

--- LibraryApp/app/src/main/java/com/turkcell/libraryapp/ui/navigation/Screen.kt
+++ LibraryApp/app/src/main/java/com/turkcell/libraryapp/ui/navigation/Screen.kt
@@ -7,4 +7,5 @@ sealed class Screen(val route: String)
     object Register : Screen("register")
     object Homepage : Screen("homepage")
     object Splash : Screen("splash")
+    object Borrows : Screen("borrows")
 }
