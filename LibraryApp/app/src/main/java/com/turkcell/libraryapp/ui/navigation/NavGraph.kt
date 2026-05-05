d6be3323a3e90578fb5f888820f4a5df9af660b6
Afumett0 <ahmeteren1905.aey@gmail.com>
2026-05-05 17:46:06 +0300

    feat: update NavGraph with Borrows route and RegisterScreen success flow

--- LibraryApp/app/src/main/java/com/turkcell/libraryapp/ui/navigation/NavGraph.kt
+++ LibraryApp/app/src/main/java/com/turkcell/libraryapp/ui/navigation/NavGraph.kt
@@ -1,23 +1,25 @@
 package com.turkcell.libraryapp.ui.navigation
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.turkcell.libraryapp.ui.screen.BorrowsScreen
 import com.turkcell.libraryapp.ui.screen.RegisterScreen
 import com.turkcell.libraryapp.ui.screen.HomeScreen
 import com.turkcell.libraryapp.ui.screen.LoginScreen
 import com.turkcell.libraryapp.ui.screen.SplashScreen
 import com.turkcell.libraryapp.ui.viewmodel.AuthViewModel
 import com.turkcell.libraryapp.ui.viewmodel.BookViewModel
+import com.turkcell.libraryapp.ui.viewmodel.BorrowViewModel
 
 @Composable
 fun NavGraph(navController: NavHostController = rememberNavController()) {
     val authViewModel: AuthViewModel = viewModel()
     val bookViewModel: BookViewModel = viewModel()
+    val borrowViewModel: BorrowViewModel = viewModel()
 
     NavHost(navController = navController, startDestination = Screen.Splash.route)
     {
@@ -56,7 +58,17 @@ fun NavGraph(navController: NavHostController = rememberNavController()) {
             authViewModel
         ) }
         composable(Screen.Homepage.route) {
-            HomeScreen(authViewModel, bookViewModel)
+            HomeScreen(
+                authViewModel,
+                bookViewModel,
+                borrowViewModel,
+                onNavigateToBorrows = {
+                    navController.navigate(Screen.Borrows.route)
+                }
+            )
+        }
+        composable(Screen.Borrows.route) {
+            BorrowsScreen(authViewModel, borrowViewModel)
         }
     }
 }
